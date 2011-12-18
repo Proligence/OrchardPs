72e8f859dc850d6da4d49c9dbce83d857ee5c3b2
Marek Dzikiewicz <dzikiewiczm@gmail.com>
2011-12-18 11:50:01 +0100

    Created script to automate mapping OrchardPs directories into Orchard source enlistment

---
+++ MapToOrchard.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+if "%1" == "" goto usage
+
+mklink /J %1\lib\powershell %~dp0lib\powershell
+mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell %~dp0src\Orchard.Web\Modules\Proligence.PowerShell
+mklink /J %1\src\Tools\Orchard.Management.PsProvider %~dp0src\Tools\Orchard.Management.PsProvider
+mklink /J %1\src\Tools\Orchard.Management.PsProvider.Tests %~dp0src\Tools\Orchard.Management.PsProvider.Tests
+mklink /J %1\src\Tools\OrchardPs %~dp0src\Tools\OrchardPs 
+goto done
+
+:usage
+echo Usage: MapToOrchard.cmd OrchardRootDir
+goto exit
+
+:done
+echo Done.
+
+:exit
