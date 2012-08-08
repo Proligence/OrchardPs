f0b6ee42c7eea3c58d7a3d49994fa1d16cd5e79b
Marek Dzikiewicz <dzikiewiczm@gmail.com>
2012-08-08 15:54:07 +0200

    Added unit tests project for Proligence.PowerShell module

--- MapToOrchard.cmd
+++ MapToOrchard.cmd
@@ -3,6 +3,7 @@
 if "%1" == "" goto usage
 
 mklink /J %1\lib\powershell %~dp0lib\powershell
+mklink /J %1\src\Orchard.Tests.Modules.PowerShell %~dp0src\Orchard.Tests.Modules.PowerShell
 mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell %~dp0src\Orchard.Web\Modules\Proligence.PowerShell
 mklink /J %1\src\Tools\Orchard.Management.PsProvider %~dp0src\Tools\Orchard.Management.PsProvider
 mklink /J %1\src\Tools\Orchard.Management.PsProvider.Tests %~dp0src\Tools\Orchard.Management.PsProvider.Tests
---
+++ src/Orchard.Tests.Modules.PowerShell/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalSuppressions.cs" company="Proligence">
+//   Copyright (c) 2011 Proligence, All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Error List, point to "Suppress Message(s)", and click 
+// "In Project Suppression File".
+// You do not need to add suppressions to this file manually.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
