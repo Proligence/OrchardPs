deb824088eee99ee0a5c6504a4535e6723278d58
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2014-05-31 21:41:03 +0200

    Refactored SignScripts.ps1 to automatically find all signable files.

--- src/SignScripts.ps1
+++ src/SignScripts.ps1
@@ -1,5 +1,6 @@
+# Get certificate from store
 $cert = Get-ChildItem Cert:\CurrentUser\My\95EF662130DEA9E1A3F07BE618659C8255920891
-Set-AuthenticodeSignature .\Orchard.Web\Modules\Proligence.PowerShell\Commands\Commands.format.ps1xml $cert
-Set-AuthenticodeSignature .\Orchard.Web\Modules\Proligence.PowerShell\Common\Common.format.ps1xml $cert
-Set-AuthenticodeSignature .\Orchard.Web\Modules\Proligence.PowerShell\Modules\Modules.format.ps1xml $cert
-Set-AuthenticodeSignature .\Orchard.Web\Modules\Proligence.PowerShell\Sites\Sites.format.ps1xml $cert
+
+# Sign files
+Get-ChildItem -Recurse -Include *.ps1xml .\Orchard.Web\Modules\Proligence.PowerShell | `
+ForEach-Object { Set-AuthenticodeSignature $_.FullName $cert }
