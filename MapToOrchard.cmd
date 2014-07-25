0c419ee43fad9ca2926aab64be2e4c7768db1b83
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2014-07-25 20:06:00 +0200

    Removed obsolete OrchardPs.exe console application.

--- MapToOrchard.cmd
+++ MapToOrchard.cmd
@@ -8,7 +8,6 @@ mklink /J %1\src\Orchard.Tests.Modules.PowerShell %~dp0src\Orchard.Tests.Modules
 mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell %~dp0src\Orchard.Web\Modules\Proligence.PowerShell
 mklink /J %1\src\Tools\Orchard.Management.PsProvider %~dp0src\Tools\Orchard.Management.PsProvider
 mklink /J %1\src\Tools\Orchard.Management.PsProvider.Tests %~dp0src\Tools\Orchard.Management.PsProvider.Tests
-mklink /J %1\src\Tools\OrchardPs %~dp0src\Tools\OrchardPs 
 mklink /J %1\src\Tools\Proligence.PowerShell.Vfs %~dp0src\PowerShellVfs\src\Proligence.PowerShell.Vfs
 mklink /J %1\src\Tools\Proligence.PowerShell.Vfs.Tests %~dp0src\PowerShellVfs\src\Proligence.PowerShell.Vfs.Tests
 goto done
