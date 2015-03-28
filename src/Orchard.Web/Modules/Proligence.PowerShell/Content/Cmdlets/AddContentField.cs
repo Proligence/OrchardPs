ac18d127ed51bddc2ad203c7ec9282c485146c34
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-03-28 17:01:37 +0100

    OPS-34: Add-ContentField cmdlet

---
+++ src/Orchard.Web/Modules/Proligence.PowerShell/Content/Cmdlets/AddContentField.cs
@@ -0,0 +1,21 @@
+﻿namespace Proligence.PowerShell.Content.Cmdlets
+{
+    using System.Management.Automation;
+    using Orchard.ContentManagement.MetaData.Builders;
+    using Proligence.PowerShell.Provider;
+
+    [CmdletAlias("acf")]
+    [Cmdlet(VerbsCommon.Add, "ContentField", DefaultParameterSetName = "Default", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Medium)]
+    public class AddContentField : AlterContentPartFieldCmdletBase
+    {
+        protected override string GetActionName(string contentFieldName, string contentPartName, string tenantName)
+        {
+            return "Add Field: " + contentFieldName + ", Part: " + contentPartName + ", Tenant: " + tenantName;
+        }
+
+        protected override void PerformAction(ContentPartDefinitionBuilder builder, string contentFieldName)
+        {
+            builder.WithField(contentFieldName);
+        }
+    }
+}
