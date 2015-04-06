4199358939dff5ed2ca871c3a51e33e85bf36953
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-04-06 14:15:34 +0200

    Add tests for legacy command management

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Commands/CommandVfsTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Commands
+{
+    using System.Linq;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class CommandVfsTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public CommandVfsTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void VfsTenantShouldContainCommands()
+        {
+            this.powerShell.Session.ProcessInput("Get-ChildItem Orchard:\\Tenants\\Default\\Commands");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.Equal("Name", table.Header[0]);
+            Assert.Equal("Help", table.Header[1]);
+            Assert.True(table.Rows.Count > 0);
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "help"));
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "help commands"));
+        }
+    }
+}
---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Commands/InvokeOrchardCommandTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Commands
+{
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class InvokeOrchardCommandTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public InvokeOrchardCommandTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldInvokeLegacyCommand()
+        {
+            this.powerShell.Session.ProcessInput("Invoke-OrchardCommand help commands");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+            Assert.Contains("List of available commands:", output);
+        }
+
+        [Fact, Integration]
+        public void ShouldInvokeLegacyCommandWithParameter()
+        {
+            this.powerShell.Session.ProcessInput("Invoke-OrchardCommand theme list /Summary:true");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+            Assert.Contains("The Theme Machine", output);
+        }
+
+        [Fact, Integration]
+        public void ShouldInvokeLegacyCommandFromObject()
+        {
+            this.powerShell.Session.ProcessInput(
+                "Get-ChildItem 'Tenants\\Default\\Commands\\help commands' | Invoke-OrchardCommand");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+            Assert.Contains("List of available commands:", output);
+        }
+    }
+}
