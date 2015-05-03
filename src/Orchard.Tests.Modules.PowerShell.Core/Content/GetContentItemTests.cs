cb8776f589e3cb5af700dfea0278839aa683c25e
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-05-03 19:53:42 +0200

    OPS-185: Get-ContentItem cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Content/GetContentItemTests.cs
@@ -0,0 +1,131 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Content
+{
+    using System.Linq;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class GetContentItemTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public GetContentItemTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldGetAllContentItems()
+        {
+            this.powerShell.Session.ProcessInput("Get-ContentItem");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.True(table.Rows.Count > 0);
+        }
+
+        [Fact, Integration]
+        public void ShouldFormatContentItemsInGrid()
+        {
+            this.powerShell.Session.ProcessInput("Get-ContentItem");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.Equal("Id", table.Header[0]);
+            Assert.Equal("ContentType", table.Header[1]);
+            Assert.Equal("Title", table.Header[2]);
+        }
+
+        [Fact, Integration]
+        public void ShouldGetContentItemById()
+        {
+            this.powerShell.Session.ProcessInput("Get-ContentItem -Id 1");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.Equal("1", table.Rows.Single()[0]);
+            Assert.Equal("Site", table.Rows.Single()[1]);
+            Assert.Empty(table.Rows.Single()[2]);
+        }
+
+        [Theory, Integration]
+        [InlineData("Get-ContentItem Layer")]
+        [InlineData("Get-ContentItem -ContentType Layer")]
+        public void ShouldGetContentItemByContentType(string command)
+        {
+            this.powerShell.Session.ProcessInput(command);
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.True(table.Rows.Count > 0);
+            Assert.All(table.Rows, row => Assert.Equal("Layer", row[1]));
+        }
+
+        [Theory, Integration]
+        [InlineData("Get-ContentItem -Id 1 -VersionOptions Latest")]
+        [InlineData("Get-ContentItem -Id 1 -VersionOptions Published")]
+        [InlineData("Get-ContentItem -Id 1 -VersionOptions Draft")]
+        [InlineData("Get-ContentItem -Id 1 -VersionOptions DraftRequired")]
+        [InlineData("Get-ContentItem -Id 1 -VersionOptions AllVersions")]
+        public void ShouldGetContentItemWithVersionOptions(string command)
+        {
+            this.powerShell.Session.ProcessInput(command);
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.True(table.Rows.Count > 0);
+            Assert.All(table.Rows, row => Assert.Equal("Site", row[1]));
+        }
+
+        [Fact, Integration]
+        public void ShouldGetContentItemWithDraftVersionOptions()
+        {
+            // NOTE:
+            // First call Get-ContentItem with DraftRequired version options to make sure a draft exists, otherwise
+            // Get-ContentItem with Draft version options may not return anything.
+            
+            var commands = new[]
+            {
+                "Get-ContentItem -Id 1 -VersionOptions DraftRequired",
+                "Get-ContentItem -Id 1 -VersionOptions Draft",
+            };
+            
+            foreach (var command in commands)
+            {
+                this.powerShell.ConsoleConnection.Reset();
+                this.powerShell.Session.ProcessInput(command);
+
+                string output = this.powerShell.ConsoleConnection.Output.ToString();
+                Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+                var table = PsTable.Parse(output);
+                Assert.True(table.Rows.Count > 0);
+                Assert.All(table.Rows, row => Assert.Equal("Site", row[1]));
+            }
+        }
+
+        [Fact, Integration]
+        public void ShouldGetContentItemByVersionNumber()
+        {
+            this.powerShell.Session.ProcessInput("Get-ContentItem -Id 1 -Version 1 | Format-Table Id, Version");
+
+            string output = this.powerShell.ConsoleConnection.Output.ToString();
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(output);
+            Assert.Equal("1", table.Rows.Single()[0]);
+            Assert.Equal("1", table.Rows.Single()[1]);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Core/Content/Cmdlets/GetContentItem.cs
@@ -0,0 +1,137 @@
+﻿namespace Proligence.PowerShell.Core.Content.Cmdlets
+{
+    using System.Collections.Generic;
+    using System.Management.Automation;
+    using Orchard.ContentManagement;
+    using Orchard.Environment.Configuration;
+    using Proligence.PowerShell.Provider;
+
+    [CmdletAlias("gcit")]
+    [Cmdlet(VerbsCommon.Get, "ContentItem", DefaultParameterSetName = "Default", ConfirmImpact = ConfirmImpact.None)]
+    public class GetContentItem : TenantCmdlet
+    {
+        [ValidateNotNullOrEmpty]
+        [Parameter(ParameterSetName = "Default", Mandatory = false, Position = 1)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false, Position = 1)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false, Position = 1)]
+        public string ContentType { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false)]
+        public int? Id { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false)]
+        public int? Version { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false)]
+        public VersionOptionsEnum? VersionOptions { get; set; }
+
+        protected override void ProcessRecord(ShellSettings tenant)
+        {
+            this.UsingWorkContextScope(
+                tenant.Name,
+                scope =>
+                {
+                    foreach (ContentItem item in this.GetContentItems(scope.Resolve<IContentManager>()))
+                    {
+                        this.WriteObject(item);
+                    }
+                });
+        }
+
+        private IEnumerable<ContentItem> GetContentItems(IContentManager contentManager)
+        {
+            if (this.Id != null)
+            {
+                var versionOptions = this.GetVersionOptions();
+                if (versionOptions != null)
+                {
+                    yield return contentManager.Get(this.Id.Value, versionOptions);
+                }
+                else
+                {
+                    yield return contentManager.Get(this.Id.Value);
+                }
+            }
+            else
+            {
+                foreach (var contentItem in this.QueryContentItems(contentManager))
+                {
+                    yield return contentItem;
+                }
+            }
+        }
+
+        private IEnumerable<ContentItem> QueryContentItems(IContentManager contentManager)
+        {
+            IContentQuery<ContentItem> query;
+
+            // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+            if (!string.IsNullOrEmpty(this.ContentType))
+            {
+                query = contentManager.Query(this.ContentType);
+            }
+            else
+            {
+                query = contentManager.Query();
+            }
+
+            var versionOptions = this.GetVersionOptions();
+            if (versionOptions != null)
+            {
+                query.ForVersion(versionOptions);
+            }
+
+            return query.List();
+        }
+
+        private VersionOptions GetVersionOptions()
+        {
+            if (this.VersionOptions == VersionOptionsEnum.Latest)
+            {
+                return Orchard.ContentManagement.VersionOptions.Latest;
+            }
+
+            if (this.VersionOptions == VersionOptionsEnum.Published)
+            {
+                return Orchard.ContentManagement.VersionOptions.Published;
+            }
+
+            if (this.VersionOptions == VersionOptionsEnum.Draft)
+            {
+                return Orchard.ContentManagement.VersionOptions.Draft;
+            }
+
+            if (this.VersionOptions == VersionOptionsEnum.DraftRequired)
+            {
+                return Orchard.ContentManagement.VersionOptions.DraftRequired;
+            }
+
+            if (this.VersionOptions == VersionOptionsEnum.AllVersions)
+            {
+                return Orchard.ContentManagement.VersionOptions.AllVersions;
+            }
+
+            if (this.Version != null)
+            {
+                return Orchard.ContentManagement.VersionOptions.Number(this.Version.Value);
+            }
+
+            return null;
+        }
+    }
+
+    public enum VersionOptionsEnum
+    {
+        Latest,
+        Published,
+        Draft,
+        DraftRequired,
+        AllVersions
+    }
+}
