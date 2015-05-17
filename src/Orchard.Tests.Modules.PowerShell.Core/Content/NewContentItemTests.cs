c0ccb7b94f5b5b0f38cab7cba003da8abb9abe23
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-05-17 22:34:11 +0200

    OPS-186: New-ContentItem cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Content/NewContentItemTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Content
+{
+    using System;
+    using System.Linq;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class NewContentItemTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public NewContentItemTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldCreateContentItem()
+        {
+            this.powerShell.Session.ProcessInput("New-ContentItem Page");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal("0", table.Rows.Single()[0]);
+            Assert.Equal("Page", table.Rows.Single()[1]);
+        }
+
+        [Fact, Integration]
+        public void ShouldCreateContentItemFromContentTypeObject()
+        {
+            this.powerShell.Session.ProcessInput("Get-ContentType Page | New-ContentItem");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal("0", table.Rows.Single()[0]);
+            Assert.Equal("Page", table.Rows.Single()[1]);
+        }
+
+        [Fact, Integration]
+        public void ShouldCreateDraftContentItem()
+        {
+            this.powerShell.Session.ProcessInput("New-ContentItem Page -Draft");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            int id = Convert.ToInt32(table.Rows.Single()[0]);
+            Assert.True(id > 0);
+            Assert.Equal("Page", table.Rows.Single()[1]);
+
+            this.powerShell.ConsoleConnection.Reset();
+            this.powerShell.Session.ProcessInput("(Get-ContentItem -Id " + id + " -VersionOptions Draft).VersionRecord.Published");
+            Assert.Equal("False", this.powerShell.ConsoleConnection.Output.ToString().Trim());
+        }
+
+        [Fact, Integration]
+        public void ShouldCreatePublishedContentItem()
+        {
+            this.powerShell.Session.ProcessInput("New-ContentItem Page -Published");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            int id = Convert.ToInt32(table.Rows.Single()[0]);
+            Assert.True(id > 0);
+            Assert.Equal("Page", table.Rows.Single()[1]);
+
+            this.powerShell.ConsoleConnection.Reset();
+            this.powerShell.Session.ProcessInput("(Get-ContentItem -Id " + id + " -VersionOptions Published).VersionRecord.Published");
+            Assert.Equal("True", this.powerShell.ConsoleConnection.Output.ToString().Trim());
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Core/Content/Cmdlets/NewContentItem.cs
@@ -0,0 +1,85 @@
+﻿namespace Proligence.PowerShell.Core.Content.Cmdlets
+{
+    using System.Management.Automation;
+    using System.Text;
+    using Orchard.ContentManagement;
+    using Orchard.ContentManagement.MetaData.Models;
+    using Orchard.Environment.Configuration;
+    using Proligence.PowerShell.Core.Content.Nodes;
+    using Proligence.PowerShell.Provider;
+
+    [CmdletAlias("ncit")]
+    [Cmdlet(VerbsCommon.New, "ContentItem", DefaultParameterSetName = "Default", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public class NewContentItem : TenantCmdlet
+    {
+        [ValidateNotNullOrEmpty]
+        [Parameter(ParameterSetName = "Default", Mandatory = true, Position = 1)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = true, Position = 1)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = true, Position = 1)]
+        public string ContentType { get; set; }
+
+        [Parameter(ParameterSetName = "Default")]
+        [Parameter(ParameterSetName = "TenantObject")]
+        [Parameter(ParameterSetName = "AllTenants")]
+        [Parameter(ParameterSetName = "ContentTypeObject")]
+        public SwitchParameter Draft { get; set; }
+
+        [Parameter(ParameterSetName = "Default")]
+        [Parameter(ParameterSetName = "TenantObject")]
+        [Parameter(ParameterSetName = "AllTenants")]
+        [Parameter(ParameterSetName = "ContentTypeObject")]
+        public SwitchParameter Published { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false)]
+        [Parameter(ParameterSetName = "ContentTypeObject", Mandatory = false)]
+        public override string Tenant { get; set; }
+
+        [ValidateNotNull]
+        [Parameter(ParameterSetName = "ContentTypeObject", Mandatory = true, ValueFromPipeline = true)]
+        public ContentTypeDefinition ContentTypeObject { get; set; }
+
+        protected override void ProcessRecord(ShellSettings tenant)
+        {
+            var contentType = this.ContentTypeObject != null ? this.ContentTypeObject.Name : this.ContentType;
+            var target = "Content Type: " + contentType + ", Tenant: " + tenant.Name;
+            
+            if (this.ShouldProcess(target, this.GetAction()))
+            {
+                this.UsingWorkContextScope(
+                    tenant.Name,
+                    scope =>
+                    {
+                        var contentManager = scope.Resolve<IContentManager>();
+                        var contentItem = contentManager.New(contentType);
+
+                        if (this.Published.ToBool())
+                        {
+                            contentManager.Create(contentItem, VersionOptions.Published);
+                        }
+                        else if (this.Draft.ToBool())
+                        {
+                            contentManager.Create(contentItem, VersionOptions.Draft);
+                        }
+
+                        this.WriteObject(ContentItemNode.BuildPSObject(contentItem));
+                    });
+            }
+        }
+
+        private string GetAction()
+        {
+            var builder = new StringBuilder("Create");
+
+            if (this.Published.ToBool())
+            {
+                builder.Append(" Published");
+            }
+            else if (this.Draft.ToBool())
+            {
+                builder.Append(" Draft");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
