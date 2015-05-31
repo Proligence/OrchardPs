b21f22e597f618748c187b6ac76a735cec1b5736
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-05-31 16:05:15 +0200

    OPS-190: Unpublish-ContentItem cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Content/UnpublishContentItemTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Content
+{
+    using System;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class UnpublishContentItemTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public UnpublishContentItemTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldUnpublishContentItemById()
+        {
+            int id = this.CreateContentItem();
+            this.powerShell.Session.ProcessInput("Unpublish-ContentItem " + id + " -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Unpublish\" on target \"Content Item: " + id + ", Version: 1, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            this.AssertContentItemNotPublished(id);
+        }
+
+        [Fact, Integration]
+        public void ShouldUnpublishContentItemByObject()
+        {
+            int id = this.CreateContentItem();
+            this.powerShell.Session.ProcessInput(
+                "Get-ContentItem -Id " + id + " -VersionOptions Latest | Unpublish-ContentItem -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Unpublish\" on target \"Content Item: " + id + ", Version: 1, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            this.AssertContentItemNotPublished(id);
+        }
+
+        private int CreateContentItem()
+        {
+            this.powerShell.Session.ProcessInput("$item = New-ContentItem Page -Published");
+            this.powerShell.Session.ProcessInput("$item.Id");
+            string output = this.powerShell.ConsoleConnection.Output.ToString().Trim();
+            this.powerShell.ConsoleConnection.Reset();
+
+            return Convert.ToInt32(output);
+        }
+
+        private void AssertContentItemNotPublished(int id)
+        {
+            this.powerShell.ConsoleConnection.Reset();
+            this.powerShell.Session.ProcessInput("(Get-ContentItem -Id " + id + " -VersionOptions Latest).VersionRecord.Published");
+            Assert.Equal("False", this.powerShell.ConsoleConnection.Output.ToString().Trim());
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Core/Content/Cmdlets/UnpublishContentItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Proligence.PowerShell.Core.Content.Cmdlets
+{
+    using System.Management.Automation;
+    using Orchard.ContentManagement;
+
+    [Cmdlet(VerbsData.Unpublish, "ContentItem", DefaultParameterSetName = "Default", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public class UnpublishContentItem : AlterContentItemCmdletBase
+    {
+        protected override string GetActionName()
+        {
+            return "Unpublish";
+        }
+
+        protected override void PerformAction(IContentManager contentManager, ContentItem contentItem)
+        {
+            contentManager.Unpublish(contentItem);
+        }
+    }
+}
