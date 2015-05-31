db2257ec1ce9e3f340db8ef66abbe3d42730cab9
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-05-31 16:24:10 +0200

    OPS-191: Copy-ContentItem cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Content/CopyContentItemTests.cs
@@ -0,0 +1,62 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Content
+{
+    using System;
+    using System.Linq;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class CopyContentItemTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public CopyContentItemTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldCopyContentItemById()
+        {
+            int id = this.CreateContentItem();
+            this.powerShell.Session.ProcessInput("Copy-ContentItem " + id + " -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Copy\" on target \"Content Item: " + id + ", Version: 1, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            var newId = Convert.ToInt32(table.Rows.Single()[0]);
+            Assert.True(newId > id);
+        }
+
+        [Fact, Integration]
+        public void ShouldCopyContentItemByObject()
+        {
+            int id = this.CreateContentItem();
+            this.powerShell.Session.ProcessInput(
+                "Get-ContentItem -Id " + id + " -VersionOptions Latest | Copy-ContentItem -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Copy\" on target \"Content Item: " + id + ", Version: 1, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            var newId = Convert.ToInt32(table.Rows.Single()[0]);
+            Assert.True(newId > id);
+        }
+
+        private int CreateContentItem()
+        {
+            this.powerShell.Session.ProcessInput("$item = New-ContentItem Page -Published");
+            this.powerShell.Session.ProcessInput("$item.Id");
+            string output = this.powerShell.ConsoleConnection.Output.ToString().Trim();
+            this.powerShell.ConsoleConnection.Reset();
+
+            return Convert.ToInt32(output);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Core/Content/Cmdlets/CopyContentItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Proligence.PowerShell.Core.Content.Cmdlets
+{
+    using System.Management.Automation;
+    using Orchard.ContentManagement;
+    using Proligence.PowerShell.Core.Content.Nodes;
+
+    [Cmdlet(VerbsCommon.Copy, "ContentItem", DefaultParameterSetName = "Default", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public class CopyContentItem : AlterContentItemCmdletBase
+    {
+        protected override string GetActionName()
+        {
+            return "Copy";
+        }
+
+        protected override void PerformAction(IContentManager contentManager, ContentItem contentItem)
+        {
+            var newContentItem = contentManager.Clone(contentItem);
+            this.WriteObject(ContentItemNode.BuildPSObject(newContentItem));
+        }
+    }
+}
