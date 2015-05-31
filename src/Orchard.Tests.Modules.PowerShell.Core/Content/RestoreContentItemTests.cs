eea01d4dcdca0bef3cf62a7224fbb744ddc9d615
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-05-31 18:57:27 +0200

    OPS-193: Restore-ContentItem cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Content/RestoreContentItemTests.cs
@@ -0,0 +1,98 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Core.Content
+{
+    using System;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class RestoreContentItemTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public RestoreContentItemTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldRestoreContentItemById()
+        {
+            int id = this.CreateContentItem();
+            this.AssertContentItemTitle(id, "baz");
+
+            this.powerShell.Session.ProcessInput("Restore-ContentItem " + id + " 2 -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Restore\" on target \"Content Item: " + id + ", Version: 2, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            this.AssertContentItemTitle(id, "bar");
+        }
+
+        [Fact, Integration]
+        public void ShouldRestoreContentItemByObject()
+        {
+            int id = this.CreateContentItem();
+            this.AssertContentItemTitle(id, "baz");
+
+            this.powerShell.Session.ProcessInput(
+                "Get-ContentItem -Id " + id + " -VersionOptions Latest | Restore-ContentItem -Version 2 -Verbose");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            Assert.Equal(
+                "Performing the operation \"Restore\" on target \"Content Item: " + id + ", Version: 2, Tenant: Default\".",
+                this.powerShell.ConsoleConnection.VerboseOutput.ToString().Trim());
+
+            this.AssertContentItemTitle(id, "bar");
+        }
+
+        [Fact, Integration]
+        public void ShouldFailWhenVersionNumberNotSpecified()
+        {
+            int id = this.CreateContentItem();
+            this.AssertContentItemTitle(id, "baz");
+
+            this.powerShell.Session.ProcessInput("Restore-ContentItem " + id + " -Verbose");
+
+            var expected = "The VersionOptions or Version parameter must be specified.";
+            Assert.True(this.powerShell.ConsoleConnection.ErrorOutput.ToString().Contains(expected));
+            this.AssertContentItemTitle(id, "baz");
+        }
+
+        private int CreateContentItem()
+        {
+            this.powerShell.Session.ProcessInput("$item = New-ContentItem Page -Draft");
+            this.powerShell.Session.ProcessInput("$item.Id");
+            int id = Convert.ToInt32(this.powerShell.ConsoleConnection.Output.ToString().Trim());
+
+            // Version 1
+            this.powerShell.Session.ProcessInput("$item.TitlePart.Title = 'foo'");
+            this.powerShell.Session.ProcessInput("Update-ContentItem $item -VersionOptions DraftRequired");
+            this.powerShell.Session.ProcessInput("Publish-ContentItem $item -VersionOptions Latest");
+
+            // Version 2
+            this.powerShell.Session.ProcessInput("$item.TitlePart.Title = 'bar'");
+            this.powerShell.Session.ProcessInput("Update-ContentItem $item -VersionOptions DraftRequired");
+            this.powerShell.Session.ProcessInput("Publish-ContentItem $item -VersionOptions Latest");
+
+            // Version 3
+            this.powerShell.Session.ProcessInput("$item.TitlePart.Title = 'baz'");
+            this.powerShell.Session.ProcessInput("Update-ContentItem $item -VersionOptions DraftRequired");
+            this.powerShell.Session.ProcessInput("Publish-ContentItem $item -VersionOptions Latest");
+            
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+            this.powerShell.ConsoleConnection.Reset();
+
+            return id;
+        }
+
+        private void AssertContentItemTitle(int id, string expectedTitle)
+        {
+            this.powerShell.ConsoleConnection.Reset();
+            this.powerShell.Session.ProcessInput("(Get-ContentItem -Id " + id + " -VersionOptions Latest).Title");
+            Assert.Equal(expectedTitle, this.powerShell.ConsoleConnection.Output.ToString().Trim());
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Core/Content/Cmdlets/RestoreContentItem.cs
@@ -0,0 +1,44 @@
+﻿namespace Proligence.PowerShell.Core.Content.Cmdlets
+{
+    using System.Management.Automation;
+    using Orchard.ContentManagement;
+    using Proligence.PowerShell.Core.Content.Nodes;
+    using Proligence.PowerShell.Provider.Utilities;
+
+    [Cmdlet(VerbsData.Restore, "ContentItem", DefaultParameterSetName = "Default", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public class RestoreContentItem : AlterContentItemCmdletBase
+    {
+        /// <remarks>
+        /// As this parameter doesn't have any parameter sets, it won't be available for the Restore-ContentItem
+        /// cmdlet. It doesn't make any since, since we need to specify a version number to restore.
+        /// </remarks>>
+        public override VersionOptionsEnum? VersionOptions { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false, Position = 2)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false, Position = 2)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false, Position = 2)]
+        [Parameter(ParameterSetName = "ContentItemObject", Mandatory = false, Position = 2)]
+        public override int? Version { get; set; }
+
+        protected override string GetActionName()
+        {
+            return "Restore";
+        }
+
+        protected override void PerformAction(IContentManager contentManager, ContentItem contentItem)
+        {
+            if (this.Version != null)
+            {
+                var versionOptions = Orchard.ContentManagement.VersionOptions.Number(this.Version.Value);
+                var restoredItem = contentManager.Restore(contentItem, versionOptions);
+                this.WriteObject(ContentItemNode.BuildPSObject(restoredItem));
+            }
+            else
+            {
+                this.WriteError(Error.InvalidArgument(
+                    "The VersionOptions or Version parameter must be specified.",
+                    "ContentItemVersionNotSpecified"));
+            }
+        }
+    }
+}
