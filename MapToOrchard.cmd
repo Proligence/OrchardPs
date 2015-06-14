97d96fbe46c49333f8c5d49c9e1c919e6355cff7
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-06-14 19:13:31 +0200

    Create Proligence.PowerShell.Recipes module

--- MapToOrchard.cmd
+++ MapToOrchard.cmd
@@ -6,10 +6,12 @@ mklink /J %1\lib\powershell %~dp0lib\powershell
 mklink /J %1\lib\xunit %~dp0lib\xunit
 mklink /J %1\src\Orchard.Tests.Modules.PowerShell.Core %~dp0src\Orchard.Tests.Modules.PowerShell.Core
 mklink /J %1\src\Orchard.Tests.Modules.PowerShell.Provider %~dp0src\Orchard.Tests.Modules.PowerShell.Provider
+mklink /J %1\src\Orchard.Tests.Modules.PowerShell.Recipes %~dp0src\Orchard.Tests.Modules.PowerShell.Recipes
 mklink /J %1\src\Orchard.Tests.Modules.PowerShell.Web %~dp0src\Orchard.Tests.Modules.PowerShell.Web
 mklink /J %1\src\Orchard.Tests.PowerShell.Infrastructure %~dp0src\Orchard.Tests.PowerShell.Infrastructure
 mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell.Core %~dp0src\Orchard.Web\Modules\Proligence.PowerShell.Core
 mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell.Provider %~dp0src\Orchard.Web\Modules\Proligence.PowerShell.Provider
+mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell.Recipes %~dp0src\Orchard.Web\Modules\Proligence.PowerShell.Recipes
 mklink /J %1\src\Orchard.Web\Modules\Proligence.PowerShell.Web %~dp0src\Orchard.Web\Modules\Proligence.PowerShell.Web
 mklink /J %1\src\Tools\OrchardPs %~dp0src\Tools\OrchardPs 
 goto done
---
+++ src/Orchard.Tests.Modules.PowerShell.Recipes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Orchard.Tests.Modules.PowerShell.Recipes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Proligence Orchard PowerShell")]
+[assembly: AssemblyCopyright("Copyright © Proligence")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4df0b70e-5830-47dd-bfbd-7e07fd1bdbd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: CLSCompliant(false)]
