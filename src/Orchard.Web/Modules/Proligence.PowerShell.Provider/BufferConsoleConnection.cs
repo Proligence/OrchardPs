942602ae5b0796171994fc68f713ef7a0ba22a4b
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-06-14 20:59:51 +0200

    Support for using PS session from code
    This changeset adds better support for using the PowerShell engine
    from code (instead of using OrchardPs.exe or the web console). It
    introduces the BufferConsoleConnection class which implements the
    IConsoleConnection interface using a set of memory buffers which may
    be manipulated from code easily. Additionlly, it adds some extension
    methods for IPsSessionManager to make it easy to create new PS sessions
    from code.

---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Provider/BufferConsoleConnection.cs
@@ -0,0 +1,75 @@
+﻿namespace Proligence.PowerShell.Provider
+{
+    using System;
+    using System.Text;
+    using Proligence.PowerShell.Provider.Console.UI;
+
+    public class BufferConsoleConnection : IConsoleConnection
+    {
+        public BufferConsoleConnection()
+        {
+            this.VerboseOutput = new StringBuilder();
+            this.WarningOutput = new StringBuilder();
+            this.DebugOutput = new StringBuilder();
+            this.ErrorOutput = new StringBuilder();
+            this.Output = new StringBuilder();
+        }
+
+        public StringBuilder VerboseOutput { get; private set; }
+        public StringBuilder WarningOutput { get; private set; }
+        public StringBuilder DebugOutput { get; private set; }
+        public StringBuilder ErrorOutput { get; private set; }
+        public StringBuilder Output { get; private set; }
+
+        public OutputData LastOutputData { get; private set; }
+
+        public void Initialize()
+        {
+        }
+
+        public void Send(string connectionId, OutputData data)
+        {
+            if (data != null)
+            {
+                this.LastOutputData = data;
+
+                if (data.Output != null)
+                {
+                    string output = data.NewLine
+                        ? data.Output + Environment.NewLine
+                        : data.Output;
+
+                    if (data.Type == OutputType.Verbose)
+                    {
+                        this.VerboseOutput.Append(output);
+                    }
+                    else if (data.Type == OutputType.Warning)
+                    {
+                        this.WarningOutput.Append(output);
+                    }
+                    else if (data.Type == OutputType.Debug)
+                    {
+                        this.DebugOutput.Append(output);
+                    }
+                    else if (data.Type == OutputType.Error)
+                    {
+                        this.ErrorOutput.Append(output);
+                    }
+                    else
+                    {
+                        this.Output.Append(output);
+                    }
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            this.VerboseOutput.Clear();
+            this.WarningOutput.Clear();
+            this.DebugOutput.Clear();
+            this.ErrorOutput.Clear();
+            this.Output.Clear();
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Provider/PsSessionManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Proligence.PowerShell.Provider
+{
+    using System;
+
+    public static class PsSessionManagerExtensions
+    {
+        public static IPsSession CreateSession(this IPsSessionManager sessionManager)
+        {
+            return sessionManager.NewSession(Guid.NewGuid().ToString(), new BufferConsoleConnection());
+        }
+
+        public static IPsSession CreateSession(this IPsSessionManager sessionManager, out BufferConsoleConnection connection)
+        {
+            connection = new BufferConsoleConnection();
+            return sessionManager.NewSession(Guid.NewGuid().ToString(), connection);
+        }
+    }
+}
