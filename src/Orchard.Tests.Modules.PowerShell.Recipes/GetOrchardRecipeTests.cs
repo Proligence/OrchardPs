766ab49041ae9e096e48b3e6b83d1ab068147bde
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-06-22 23:46:12 +0200

    OPS-127: Get-OrchardRecipe cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Recipes/GetOrchardRecipeTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Orchard.Tests.Modules.PowerShell.Recipes
+{
+    using System.Linq;
+    using Orchard.Tests.PowerShell.Infrastructure;
+    using Xunit;
+
+    [Collection("PowerShell")]
+    public class GetOrchardRecipeTests : IClassFixture<PowerShellFixture>
+    {
+        private readonly PowerShellFixture powerShell;
+
+        public GetOrchardRecipeTests(PowerShellFixture powerShell)
+        {
+            this.powerShell = powerShell;
+            this.powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldGetAllRecipes()
+        {
+            this.powerShell.Session.ProcessInput("Get-OrchardRecipe");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal("Name", table.Header[0]);
+            Assert.Equal("Version", table.Header[1]);
+            Assert.Equal("Description", table.Header[2]);
+            Assert.True(table.Rows.Count > 0);
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Blog"));
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Core"));
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Default"));
+        }
+
+        [Fact, Integration]
+        public void ShouldGetRecipesByName()
+        {
+            this.powerShell.Session.ProcessInput("Get-OrchardRecipe Core");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal("Name", table.Header[0]);
+            Assert.Equal("Version", table.Header[1]);
+            Assert.Equal("Description", table.Header[2]);
+            Assert.Equal(1, table.Rows.Count);
+            Assert.Equal("Core", table[0, "Name"]);
+        }
+
+        [Fact, Integration]
+        public void ShouldGetRecipesByPartialName()
+        {
+            this.powerShell.Session.ProcessInput("Get-OrchardRecipe C*");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal("Name", table.Header[0]);
+            Assert.Equal("Version", table.Header[1]);
+            Assert.Equal("Description", table.Header[2]);
+            Assert.Equal(1, table.Rows.Count);
+            Assert.True(table.Rows.Count > 0);
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Core"));
+            Assert.All(table.Rows, r => Assert.StartsWith("C", r[0]));
+        }
+
+        [Fact, Integration]
+        public void ShouldGetRecipesByExtensionId()
+        {
+            this.powerShell.Session.ProcessInput("Get-OrchardRecipe -ExtensionId Orchard.Setup");
+            Assert.Empty(this.powerShell.ConsoleConnection.ErrorOutput.ToString());
+
+            var table = PsTable.Parse(this.powerShell.ConsoleConnection.Output.ToString());
+            Assert.Equal(3, table.Rows.Count);
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Blog"));
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Core"));
+            Assert.Equal(1, table.Rows.Count(x => x[0] == "Default"));
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Proligence.PowerShell.Recipes/Cmdlets/GetOrchardRecipe.cs
@@ -0,0 +1,73 @@
+﻿namespace Proligence.PowerShell.Recipes.Cmdlets
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using Orchard.Environment.Configuration;
+    using Orchard.Environment.Extensions;
+    using Orchard.Environment.Extensions.Models;
+    using Orchard.Recipes.Models;
+    using Orchard.Recipes.Services;
+    using Proligence.PowerShell.Provider;
+    using Proligence.PowerShell.Provider.Utilities;
+
+    [Cmdlet(VerbsCommon.Get, "OrchardRecipe", DefaultParameterSetName = "Default", ConfirmImpact = ConfirmImpact.None)]
+    public class GetOrchardRecipe : TenantCmdlet
+    {
+        /// <remarks>
+        /// This parameter doesn't make any sense for this cmdlet.
+        /// </remarks>>
+        public override SwitchParameter AllTenants { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = false, Position = 1)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false, Position = 1)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false)]
+        public string Name { get; set; }
+
+        [Alias("e")]
+        [Parameter(ParameterSetName = "Default", Mandatory = false)]
+        [Parameter(ParameterSetName = "TenantObject", Mandatory = false)]
+        [Parameter(ParameterSetName = "AllTenants", Mandatory = false)]
+        public string ExtensionId { get; set; }
+
+        protected override void ProcessRecord(ShellSettings tenant)
+        {
+            this.UsingWorkContextScope(tenant.Name, scope =>
+            {
+                var extensionManager = scope.Resolve<IExtensionManager>();
+                var harvester = scope.Resolve<IRecipeHarvester>();
+
+                foreach (ExtensionDescriptor extension in this.GetExtensions(extensionManager))
+                {
+                    foreach (Recipe recipe in harvester.HarvestRecipes(extension.Id))
+                    {
+                        if (string.IsNullOrEmpty(this.Name) || recipe.Name.WildcardEquals(this.Name))
+                        {
+                            this.WriteObject(recipe);
+                        }
+                    }
+                }
+            });
+        }
+
+        private IEnumerable<ExtensionDescriptor> GetExtensions(IExtensionManager extensionManager)
+        {
+            if (this.ExtensionId != null)
+            {
+                ExtensionDescriptor extension = extensionManager.GetExtension(this.ExtensionId);
+                if (extension == null)
+                {
+                    this.WriteError(Error.InvalidArgument(
+                        "Failed to find extension with ID '" + this.ExtensionId + "'.",
+                        "FailedToFindExtension"));
+
+                    return Enumerable.Empty<ExtensionDescriptor>();
+                }
+
+                return new[] { extension };
+            }
+            
+            return extensionManager.AvailableExtensions().ToArray();
+        }
+    }
+}
