f2bd6350ac9bb0a870007b983cce7576ae3d5a30
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-09-13 20:34:17 +0200

    OPS-82: Clear-OrchardCache cmdlet

---
+++ src/Orchard.Tests.Modules.PowerShell.Core/Caching/ClearOrchardCacheTests.cs
@@ -0,0 +1,26 @@
+﻿using Orchard.Tests.PowerShell.Infrastructure;
+using Xunit;
+
+namespace Orchard.Tests.Modules.PowerShell.Core.Caching {
+    [Collection("PowerShell")]
+    public class ClearOrchardCacheTests : IClassFixture<PowerShellFixture> {
+        private readonly PowerShellFixture _powerShell;
+
+        public ClearOrchardCacheTests(PowerShellFixture powerShell) {
+            _powerShell = powerShell;
+            _powerShell.ConsoleConnection.Reset();
+        }
+
+        [Fact, Integration]
+        public void ShouldClearOrchardCache() {
+            var output = _powerShell.Execute("Clear-OrchardCache");
+            Assert.Empty(output);
+        }
+
+        [Fact, Integration]
+        public void ClearOrchardCacheShouldSupportWhatIf() {
+            var output = _powerShell.Execute("Clear-OrchardCache -WhatIf");
+            Assert.Equal("What if: Performing the operation \"Clear\" on target \"OrchardCache\".", output);
+        }
+    }
+}
